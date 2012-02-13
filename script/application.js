edc25559ae7bbefea9b53b9711cb69271a9676dc
Mike Ferchak <mikeferchak@gmail.com>
2012-02-13 00:12:31 -0500

    New files, mixins, flickr, and animations. Holy crap.

--- script/application.js
+++ script/application.js
@@ -1,17 +1,38 @@
+var backgroundURL;
+
+getBackgroundPhoto();
+
+function getBackgroundPhoto(){
+    var apiKey = 'b2969414c7c7419adb001b18f881a371';
+    var url = 'http://api.flickr.com/services/rest/?&method=flickr.photosets.getPhotos&api_key='+ apiKey +'&user_id=mikeferchak&photoset_id=72157629217619129&format=json&privacy_filter=1&media=photos&jsoncallback=?';
+    $.getJSON(url, function(data){
+        console.log(data);
+        var randomItem = Math.floor(data.photoset.photo.length * Math.random());
+        var item = data.photoset.photo[randomItem];
+        var backgroundURL = 'http://farm'+ item.farm +'.staticflickr.com/'+item.server+'/'+item.id+'_'+item.secret+'_b.jpg';
+    });
+}
+
+
 $(document).ready(function() {
   var originalHeaderHeight = parseInt($("header h1").css("font-size"), 10);
   var originalHeaderWidth = $("header h1").innerWidth();
   var headerAspect = originalHeaderHeight / originalHeaderWidth;
   var lsz = 2000; // Default height in px of the light source
 
-  rotateCrap();
+  // rotateCrap();
   doThings();
+  setBackgroundPhoto();
 
   // Initialize all of the things
   function doThings(){
     responsiveHeader();
     repositionlightsource();
     loopthroughallshadowelements();
+  }
+
+  function setBackgroundPhoto(){
+    $('body').css('background-image', 'url('+pictureUrl+')');
   }
 
   // Do things when window is resized
@@ -34,16 +55,16 @@ $(document).ready(function() {
       resizeHeader(1); // normal screen
     }
     else if ($(window).width() > 400) {
-      resizeHeader(2); // ipad
+      resizeHeader(2.2); // ipad
     }
     else {
-      resizeHeader(3); // iphone
+      resizeHeader(3.6); // iphone
     }
   }
 
   // Resizing that crap automatically
   function resizeHeader(sizeAdjustmentMultiplier){
-    newHeaderHeight = headerAspect * $(window).width() * sizeAdjustmentMultiplier;
+    newHeaderHeight = headerAspect * $("header").innerWidth() * sizeAdjustmentMultiplier;
     $("header h1").css({"font-size": newHeaderHeight});
   }
 
@@ -95,22 +116,34 @@ $(document).ready(function() {
     var shadowvalue = -(bxoffset) + "px " + -(byoffset) + "px " + blur + "px rgba(0,0,0,"+darkness+")";
 
     if( type === "inset" )  {
-      $(element).css({'box-shadow': 'inset '+shadowvalue, 'background-image': awesomeGradient("0.6","0.2")});
+      $(element).css({'box-shadow': 'inset '+shadowvalue, 'background-image': awesomeGradient("0.6","0.1")});
     }
     if(type === "box")  {
-      $(element).css({'box-shadow': 'inset '+ shadowvalue, 'background-image': awesomeGradient("1","0.6")});
+      $(element).css({'box-shadow': 'inset '+ shadowvalue, 'background-image': awesomeGradient("0.8","0.2")});
     }
     if(type === "text")  {
-      $(element).css('text-shadow',shadowvalue);
+      $(element).css({'text-shadow': shadowvalue});
     }
-
     function awesomeGradient(opacity1, opacity2) {
       if(!opacity1) {opacity = "1";}
       if(!opacity2) {opacity = "0.6";}
       return(
-        '-webkit-radial-gradient('+(lsx - offset.left)+'px '+(lsy - offset.top)+'px, 300px 300px, rgba(255, 255, 255, '+opacity1+'), rgba(247, 247, 247, '+opacity2+'))'
+        '-webkit-radial-gradient('+(lsx - offset.left)+'px '+(lsy - offset.top)+'px, 200px 200px, rgba(249, 248, 245, '+opacity1+'), rgba(247, 247, 247, '+opacity2+'))'
       );
     }
+  }
+
+  function setBackgroundPhoto(){
+    var apiKey = 'b2969414c7c7419adb001b18f881a371';
+    var url = 'http://api.flickr.com/services/rest/?&method=flickr.photosets.getPhotos&api_key='+ apiKey +'&user_id=mikeferchak&photoset_id=72157629217619129&format=json&privacy_filter=1&media=photos&jsoncallback=?';
+    $.getJSON(url, function(data){
+        console.log(data);
+        var randomItem = Math.floor(data.photoset.photo.length * Math.random());
+        var item = data.photoset.photo[randomItem];
+        var pictureUrl = 'http://farm'+ item.farm +'.staticflickr.com/'+item.server+'/'+item.id+'_'+item.secret+'_b.jpg';
+        $('body').css('background-image', 'url('+pictureUrl+')');
+        //$('img#background').attr('src', pictureUrl).addClass('active');
+    });
   }
 
 });

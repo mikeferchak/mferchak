6423d6cc3e66cd1f08ee10edd71e2b98df50f75e
Mike Ferchak <mikeferchak@gmail.com>
2012-03-27 16:38:41 -0400

    About time I did this.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mferchak::Application.config.secret_token = '1bc6db3cd68c97695f62ca64c8e33663b8a310fd51e8c9aef89a7c554461e9d3b98fd436b2bdbbe49c3c1148b939567fea549c8cd2748c4633ed02557e531ee2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Mferchak::Application.config.session_store :cookie_store, :key => '_mferchak_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Mferchak::Application.config.session_store :active_record_store
